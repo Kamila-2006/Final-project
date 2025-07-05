f0c1159dea53580d142207bfdba24fa390d36ba2
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-07-05 22:36:30 +0500

    добавлена модель user и users manager

---
+++ apps/accounts/managers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.base_user import BaseUserManager
+from rest_framework.exceptions import ValidationError
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, password, phone_number=None, **extra_fields):
+        if phone_number is None:
+            raise ValidationError("The given phone number must be set")
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        phone_number = extra_fields.pop("phone_number", None)
+        password = extra_fields.pop("password")
+        return self._create_user(phone_number=phone_number, password=password, **extra_fields)
+
+    def create_superuser(self, password, phone_number=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValidationError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValidationError("Superuser must have is_superuser=True.")
+
+        return self._create_user(phone_number=phone_number, password=password, **extra_fields)
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    ROLE_CHOICES = [
+        ('super_admin', 'Super Admin'),
+        ('admin', 'Admin'),
+        ('seller', 'Seller'),
+    ]
+
+    full_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=13)
+    profile_photo = models.ImageField(upload_to='profile-photos/', null=True, blank=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='seller')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = ["full_name"]
+
+    def __str__(self):
+        return self.full_name
+
+class Address(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='address')
+    name = models.TextField()
+    lat = models.FloatField()
+    long = models.FloatField()
